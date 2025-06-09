fed313845e7b0fe19ce544fcb48412f614038507
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2025-06-09 23:07:08 +0300

    Add entities

---
+++ BlogSystem/Domain/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogSystem.Domain.Models
+{
+    public class Post
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsPublished { get; set; } = false;
+        public List<string> Tags { get; set; } = [];
+    }
+}
---
+++ BlogSystem/Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogSystem.Domain.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string ProfilePictureUrl { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLogin { get; set; }
+        public bool IsActive { get; set; }
+        public string Role { get; set; } = string.Empty; // e.g., Admin, Editor, Viewer
+    }
+}
