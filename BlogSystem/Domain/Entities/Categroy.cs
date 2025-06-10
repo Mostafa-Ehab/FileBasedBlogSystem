1915e22da417fbc62236ede22405590305cc701b
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2025-06-10 21:00:03 +0300

    remove categroy id

--- BlogSystem/Domain/Entities/Categroy.cs
+++ BlogSystem/Domain/Entities/Categroy.cs
@@ -2,7 +2,6 @@ namespace BlogSystem.Domain.Entities
 {
     public class Category
     {
-        public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
